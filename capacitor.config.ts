9014aedb7f257c28b057b33fb23601770f9e6d91
Cesar Nuñez <alejoo2310@gmail.com>
2022-06-02 18:03:05 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Youtube-Player',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
